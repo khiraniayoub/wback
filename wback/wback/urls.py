d3107aa9a0fc598afe7da6677e4ffed5d27e722c
Juan Jose Aparicio Aparicio <aparicio_juan@hotmail.com>
2025-12-18 19:36:25 +0100

    Added static and url to static!

--- wback/wback/urls.py
+++ wback/wback/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.users.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
