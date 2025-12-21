d970fbfc780831bcdd374f444539beaf91aa2f4f
Ayoub <ayoub_bernard@hotmail.com>
2025-12-21 13:01:01 +0100

    fixing uv environment

--- wback/wback/urls.py
+++ wback/wback/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    #path('api/message/', include('apps.message.urls')),
 ]
 
 if settings.DEBUG:
